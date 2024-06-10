b2ce9ac208eb752f9ca18e953abcc810f4125372
Dmitry Zabanov <110913076+ZDM85@users.noreply.github.com>
2024-06-10 11:16:17 +0800

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import React, { useEffect } from "react";
 import "./App.css";
 
-const tg = window.Telegram.WebApp;
+
 
 function App() {
   useEffect(() => {
     tg.ready();
   }, []);
 
-  const onClose = () => {
-    tg.close();
-  };
+
 
   return (
     <div className="App">
       <div>work</div>
-      <button onClick={onClose}>Закрыть</button>
+   
     </div>
   );
 }
