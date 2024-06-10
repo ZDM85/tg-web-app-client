dec89a242eda5aedaa01c2fa50a50562cbbfefad
Dmitry Zabanov <110913076+ZDM85@users.noreply.github.com>
2024-06-10 10:43:49 +0800

    initial

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import "./App.css";
+
+const tg = window.Telegram.WebApp;
+
+function App() {
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
+  const onClose = () => {
+    tg.close();
+  };
+
+  return (
+    <div className="App">
+      <div>work</div>
+      <button onClick={onClose}>Закрыть</button>
+    </div>
+  );
+}
+
+export default App;
